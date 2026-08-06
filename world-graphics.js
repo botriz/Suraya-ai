d14a0b6c02c1f477fb7b9c553cdfd0abc360fdf2
botriz <tabbotriz@gmail.com>
2026-08-06 19:11:43 +0330

    Add SurayaGraphics object with tiles and characters

---
+++ world-graphics.js
@@ -0,0 +1,14 @@
+const SurayaGraphics = {
+    tiles: {
+        grass: "images/tiles/grass.png",
+        farm: "images/tiles/farm.png",
+        house: "images/buildings/house.png",
+        market: "images/buildings/market.png"
+    },
+
+    characters: {
+        villager: "images/characters/villager.png",
+        worker: "images/characters/worker.png",
+        farmer: "images/characters/farmer.png"
+    }
+};
