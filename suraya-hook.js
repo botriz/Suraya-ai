5ab2fae812a4dcae3b75bcecfc32fd942c5dfde7
Ahmad <313136093+botriz@users.noreply.github.com>
2026-08-20 11:13:55 +0330

    Update suraya-hook.js

--- suraya-hook.js
+++ suraya-hook.js
@@ -11,3 +11,16 @@ export function surayaBoot(rawKeyPath) {
 
   return core;
 }
+import fs from "fs";
+import { suraya } from "suraya-global-intelligence-core/suraya-master.js";
+
+export function surayaBoot(rawKeyPath) {
+  let keyBuf = null;
+  if (rawKeyPath && fs.existsSync(rawKeyPath)) {
+    keyBuf = fs.readFileSync(rawKeyPath);
+  }
+
+  const core = suraya(keyBuf);
+
+  return core;
+}
