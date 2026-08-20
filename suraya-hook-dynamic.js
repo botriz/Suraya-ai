fa4d3b40a2b220f54f29b3372e6c38c196710041
Ahmad <313136093+botriz@users.noreply.github.com>
2026-08-20 11:15:54 +0330

    Update suraya-hook-dynamic.js

--- suraya-hook-dynamic.js
+++ suraya-hook-dynamic.js
@@ -3,3 +3,8 @@ import { suraya } from "suraya-global-intelligence-core/suraya-master.js";
 export function surayaDynamic(rawKeyBuffer) {
   return suraya(rawKeyBuffer);
 }
+import { suraya } from "suraya-global-intelligence-core/suraya-master.js";
+
+export function surayaDynamic(rawKeyBuffer) {
+  return suraya(rawKeyBuffer);
+}
